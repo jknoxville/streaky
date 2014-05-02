d6d37ae396ee8c23f247b4acea3bac8ceffb1c79
John Knox <knoxjohn@amazon.com>
2014-05-03 00:48:33 +0100

    Add strings for StreakUnit types

--- lib/src/com/jknoxville/streaky/lib/event/StreakUnit.java
+++ lib/src/com/jknoxville/streaky/lib/event/StreakUnit.java
@@ -1,5 +1,15 @@
 package com.jknoxville.streaky.lib.event;
 
 public enum StreakUnit {
-    DAY, WEEK, MONTH, YEAR;
+    DAY("Day"), WEEK("Week"), MONTH("Month"), YEAR("Year");
+    private String singular, plural;
+    
+    private StreakUnit(String sing) {
+        this.singular = sing;
+        this.plural = sing+"s";
+    }
+    
+    public String getString(int amount) {
+        return amount == 1 ? this.singular : this.plural;
+    }
 }
